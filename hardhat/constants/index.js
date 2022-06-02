99a68fa3936ca79775660b915fc8703063fdba85
AbdullahGumi <abdulgumi77@gmail.com>
2022-06-02 11:49:46 +0100

    Include all project

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x9ccd3b63c725459bd85abaece0fef33ed6074092";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
